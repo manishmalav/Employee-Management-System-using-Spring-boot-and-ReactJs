9e84dffbafd821515bea711577a45cd3afeb1e4e
Manish Malav <malavm206@gmail.com>
2022-09-27 22:55:09 +0530

    MySql database connected

---
+++ src/main/java/com/manish/springbootbackend/model/Employee.java
@@ -0,0 +1,60 @@
+package com.manish.springbootbackend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    private long id;
+    private String FirstName;
+    private String LastName;
+    private String emailId;
+
+    public Employee()
+    {
+
+    }
+
+    public Employee(String firstName, String lastName, String emailId) {
+        super();
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.emailId = emailId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
