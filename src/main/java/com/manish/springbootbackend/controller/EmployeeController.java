f9582fd09699ffd00c33ca1beaf6c5ea5212191b
Manish Malav <malavm206@gmail.com>
2022-09-29 21:07:22 +0530

    Controller and Exception file added

---
+++ src/main/java/com/manish/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.manish.springbootbackend.controller;
+
+
+import com.manish.springbootbackend.model.Employee;
+import com.manish.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees ()
+    {
+        return employeeRepository.findAll();
+    }
+
+}
