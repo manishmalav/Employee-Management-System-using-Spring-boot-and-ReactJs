205de6399c79a86cdbe5aae4f969c2886f0e5e31
Manish Malav <malavm206@gmail.com>
2022-10-03 17:21:06 +0530

    Delete , Add and GET Rest API has been created

--- src/main/java/com/manish/springbootbackend/controller/EmployeeController.java
+++ src/main/java/com/manish/springbootbackend/controller/EmployeeController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
- @CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -49,6 +51,19 @@ public class EmployeeController {
 
          Employee updatedEmployee = employeeRepository.save(employee);
          return ResponseEntity.ok(updatedEmployee);
+     }
+
+     @DeleteMapping("/employees/{id}")
+     public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id)
+     {
+
+         Employee employee = employeeRepository.findById(id)
+                 .orElseThrow(() -> new ResourceNotFoundException("Employee is not exist with id" + id));
+         employeeRepository.delete(employee);
+         Map<String, Boolean> response = new HashMap<>();
+         response.put("deleted" , Boolean.TRUE);
+         return ResponseEntity.ok(response);
+
      }
 
  }
